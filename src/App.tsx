0a54bf58a04beaa9278fffff906796b0a02c692f
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2021-07-27 10:50:55 -0400

    feat: add styles default

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import './App.css'
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <div>Template Guilherme Fontes</div>
+      <div>
+        <b>Kamity </b>
+        Template
+      </div>
     </div>
   )
 }
